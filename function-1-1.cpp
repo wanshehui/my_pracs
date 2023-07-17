a37e3a63020744b992f14efa10d1bdc93bf51df3
wanshehui <a1687373@adelaide.edu.au>
2023-07-18 01:31:44 +0800

    removed comments

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,10 +1,5 @@
 #include<iostream>
 void hello(){
 
-
-    //std is a namespace
-    //Think about it as a declarative region inside which things are defined.
-    //std::cout<<“abc\n”;this is equivalent to print ("abc") with \n for a new line.
-    //cout is define in the namespace std
-    //use cout to print "Object-Oriented Programming" followed by a new line.
+ std::cout<<"Object-Oriented Programming\n";
 }
